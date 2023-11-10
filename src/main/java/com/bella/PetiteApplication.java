66d23cc099ef3b4c092568e79b91f44d8bd10aa9
Bellamy <bdanbellamy@gmail.com>
2023-11-10 13:37:21 +0200

    Beginnings

---
+++ src/main/java/com/bella/PetiteApplication.java
@@ -0,0 +1,13 @@
+package com.bella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bella/PetiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

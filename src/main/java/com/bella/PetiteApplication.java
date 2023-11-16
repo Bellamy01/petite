44ad41c94180795b2d93066eb972910fb2e10ee5
Bellamy <bdanbellamy@gmail.com>
2023-11-16 16:51:51 +0200

    JSON for Java

--- src/main/java/com/bella/PetiteApplication.java
+++ src/main/java/com/bella/PetiteApplication.java
@@ -18,6 +18,33 @@ public class PetiteApplication {
         return new GreetResponse("Hello");
     }
 
-    private record GreetResponse(String greet) {}
+    //private record GreetResponse(String greet) {}
+
+    public static class GreetResponse {
+        private final String greet;
+
+        public GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        @Override
+        public String toString() {
+            return "Greet Response {" + "greet = '" + greet + "'}";
+        }
+    }
 
 }
